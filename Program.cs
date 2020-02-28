e5891b372a9ab5305a0d7320b1480580890bbe45
Spencer Truett <srtruett1993@gmail.com>
2020-02-28 15:47:50 -0600

    Phase 2

--- Program.cs
+++ Program.cs
@@ -6,31 +6,35 @@ namespace PlanYourHeist {
         static void Main (string[] args) {
             List<Dictionary<string, string>> listOfTeamMembers = new List<Dictionary<string, string>> ();
 
-            Dictionary<string, string> teamMember = new Dictionary<string, string> ();
+            while (true) {
 
-            Console.WriteLine ("Plan Your Heist!");
-            Console.WriteLine ("Please Enter a Name...");
-            string teamMemberName = Console.ReadLine ();
-            teamMember.Add ("Name", teamMemberName);
+                Dictionary<string, string> teamMember = new Dictionary<string, string> ();
+                Console.WriteLine ("Plan Your Heist!");
+                Console.WriteLine ("Please Enter a Name...");
+                string teamMemberName = Console.ReadLine ();
+                teamMember.Add ("Name", teamMemberName);
 
-            Console.WriteLine ("Please Enter their Skill Level (1 - 10)...");
-            string teamMemberSkill = Console.ReadLine ();
-            teamMember.Add ("Skill Level", teamMemberSkill);
+                if (teamMemberName != "") {
+                    Console.WriteLine ("Please Enter their Skill Level (1 - 10)...");
+                    string teamMemberSkill = Console.ReadLine ();
+                    teamMember.Add ("Skill", teamMemberSkill);
 
-            Console.WriteLine ("Please Enter their Courage Factor (0.0 - 2.0)...");
-            string teamMemberCourage = Console.ReadLine ();
-            teamMember.Add ("Courage", teamMemberCourage);
+                    Console.WriteLine ("Please Enter their Courage Factor (0.0 - 2.0)...");
+                    string teamMemberCourage = Console.ReadLine ();
+                    teamMember.Add ("Courage", teamMemberCourage);
 
-            listOfTeamMembers.Add (teamMember);
-
-            foreach (Dictionary<string, string> member in listOfTeamMembers) {
-                // Iterate the KeyValuePairs of the Dictionary
-                foreach (KeyValuePair<string, string> memberInfo in member) {
-                    Console.WriteLine ($"{memberInfo.Key}: {memberInfo.Value}");
+                    listOfTeamMembers.Add (teamMember);
+                } else {
+                    foreach (Dictionary<string, string> member in listOfTeamMembers) {
+                        // Iterate the KeyValuePairs of the Dictionary
+                        foreach (KeyValuePair<string, string> memberInfo in member) {
+                            Console.WriteLine ($"{memberInfo.Key}: {memberInfo.Value}");
+                        }
+                    }
+                    break;
                 }
 
             }
-
         }
     }
 }
