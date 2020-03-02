b6d51f4e87ebf57d1510ba5bc358cc8ef962540b
Spencer Truett <srtruett1993@gmail.com>
2020-03-02 15:01:10 -0600

    Refactor

--- Program.cs
+++ Program.cs
@@ -14,30 +14,32 @@ namespace PlanYourHeist
       {
 
         Dictionary<string, string> teamMember = new Dictionary<string, string>();
-        Console.WriteLine("Please Enter a Name (Or leave blank to move on)...");
+        Console.Write("Please Enter a Name (Or leave blank to move on): ");
         string teamMemberName = Console.ReadLine();
         teamMember.Add("Name", teamMemberName);
 
         if (teamMemberName != "")
         {
-          Console.WriteLine("Please Enter their Skill Level (1 - 10)...");
+          Console.Write("Please Enter their Skill Level (1 - 20): ");
           string teamMemberSkill = Console.ReadLine();
           teamMember.Add("Skill", teamMemberSkill);
 
-          Console.WriteLine("Please Enter their Courage Factor (0.0 - 2.0)...");
+          Console.Write("Please Enter their Courage Factor (0.0 - 2.0): ");
           string teamMemberCourage = Console.ReadLine();
           teamMember.Add("Courage", teamMemberCourage);
 
           listOfTeamMembers.Add(teamMember);
+
+          Console.Clear();
         }
         else
         {
-          Console.WriteLine("Please Enter the Bank's Difficulty (1-100)...");
+          Console.Write("Please Enter the Bank's Difficulty (1-100): ");
           string bankDifficultyLevel = Console.ReadLine();
           int bankNumber = int.Parse(bankDifficultyLevel);
           int combinedSkillLevel = 0;
 
-          Console.WriteLine("How many times would you like to run the scenario?");
+          Console.Write("How many times would you like to run the scenario? ");
           string trialRuns = Console.ReadLine();
           int trialRunsNumber = int.Parse(trialRuns);
           int successfulRun = 0;
@@ -50,29 +52,32 @@ namespace PlanYourHeist
             combinedSkillLevel += skillNumber;
           }
 
+          Console.Clear();
           for (int i = 0; i < trialRunsNumber; i++)
           {
             Random rand = new Random();
             int luckValue = rand.Next(-10, 11);
-            bankNumber += luckValue;
+            int totalBankDifficulty = bankNumber + luckValue;
 
             Console.WriteLine($"Team's Combined Skill: {combinedSkillLevel}");
-            Console.WriteLine($"Bank Difficulty: {bankNumber}");
+            Console.WriteLine($"Bank Difficulty: {totalBankDifficulty}");
             Console.WriteLine($"Luck Value: {luckValue}");
 
-            if (combinedSkillLevel >= bankNumber)
+            if (combinedSkillLevel >= totalBankDifficulty)
             {
               Console.WriteLine("You could pull off this heist!");
+              Console.WriteLine("----------");
               successfulRun += 1;
             }
             else
             {
               Console.WriteLine("Not a chance. Recruit some more and try again.");
+              Console.WriteLine("----------");
               unsuccessfulRun += 1;
             }
-
-            Console.WriteLine($"You succeeded {successfulRun} times! And Failed {unsuccessfulRun} times!");
           }
+          Console.WriteLine("--------------------------------------");
+          Console.WriteLine($"You succeeded {successfulRun} times! And Failed {unsuccessfulRun} times!");
           break;
         }
 
